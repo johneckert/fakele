ffa6845be63817b673e13f6f1f95a8f1806cf3af
John Eckert <johnteckert@gmail.com>
2022-01-22 19:08:11 -0500

    init

---
+++ fakele.js
@@ -0,0 +1,36 @@
+function generateFakele() {
+  const tiles = ["&#11035", "&#11035", "&#11035", "&#129001", "&#129000"];
+  let board = [];
+  let tries = Math.floor(Math.random() * 5);
+
+  while (board.length <= tries) {
+    board.push(createRow());
+  }
+  let lastRow = ["&#129001", "&#129001", "&#129001", "&#129001", "&#129001"];
+  board.push(lastRow);
+  return board;
+
+  function createRow() {
+    let row = [];
+    while (row.length < 5) {
+      row.push(tiles[Math.floor(Math.random() * tiles.length)]);
+    }
+    return row;
+  }
+}
+
+function render() {
+
+  let fakele = generateFakele();
+  let main = document.getElementById("fakele");
+  main.innerHTML = ""
+  fakele.forEach(function (rowArr) {
+    let row = document.createElement("div")
+    rowArr.forEach(function (square) {
+      let spanSq = document.createElement("span")
+      spanSq.innerHTML = square;
+      row.appendChild(spanSq);
+    });
+    main.appendChild(row);
+  });
+}
